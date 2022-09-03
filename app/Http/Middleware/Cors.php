738d0ad55c8371d3b58020464446ea61b0970fb2
QuocTuanIT <kutuanonline199@gmail.com>
2022-09-03 11:49:05 +0700

    Trust Host Heroku when deploy

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,9 +16,8 @@ class Cors
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request);
-        header('Access-Control-Allow-Origin:  https://code-thoi.herokuapp.com');
-        header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Authorization, Origin');
-        header('Access-Control-Allow-Methods:  POST, PUT');
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     }
 }
