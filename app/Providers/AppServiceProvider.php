e6ffdad3ff65ac9a970badb59330b80adefe87c8
QuocTuanIT <kutuanonline199@gmail.com>
2022-09-03 12:02:18 +0700

    Trust Host Heroku when deploy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        URL::forceSchema("https");
+        if ($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 }
