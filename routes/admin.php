22e21b8635ec3276c6fce6f1fd0c3a77d919f4d7
TuanDev <kutuanonline199@gmail.com>
2022-09-08 15:23:12 +0700

    Add archive item - force delete items

--- routes/admin.php
+++ routes/admin.php
@@ -7,7 +7,11 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('dashboard', DashboardController::class,)->name('dashboard');
-Route::get('categories/{id}/restore', [CategoryController::class, 'restore'])->name('categories.restore');
+
+Route::get('categories/{id}/restore', [CategoryController::class, 'restore'])->name('categories.get.restore');
+Route::post('categories/{id}/restore', [CategoryController::class, 'restore'])->name('categories.post.restore');
+Route::post('categories/{id}/force_delete', [CategoryController::class, 'forceDelete'])->name('categories.force_delete');
+
 Route::resource('categories', CategoryController::class);
 
 
